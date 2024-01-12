32e711595b750e785f2aa0d05f461860851d9251
Isaac Javis Otim <javotim37@gmail.com>
2024-01-12 18:18:07 +0300

    Fri, Jan 12

---
+++ html-css-js-pojects/todo/script.js
@@ -0,0 +1,24 @@
+// const form = document.getElementById("form");
+// const input = document.getElementById("input");
+// const todos_ul = document.getElementById("todos_ul");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   addTodo();
+// });
+
+// function addTodo(todo) {
+//   let todoText = input.value;
+//   if (todo) {
+//     todoText = todo.text;
+//   }
+//   console.log(todoText);
+//   if (todoText) {
+//     const todoEl = document.createElement("li");
+//     if (todo && todoEl.completed) {
+//       todoEl.classList.add("completed");
+//     }
+//     todoEl.innerText = todoText;
+//     todos_ul.appendChild(todoEl);
+//   }
+// }
